279c35ab903f1e469b7b334bae2e20389e4eaaf2
VInícius <viniciultds2019@gmail.com>
2021-03-13 00:04:25 -0300

    Added a layout for the books and the design options to opt between firebase or local storage. Still have to implement the logic to choose between firebase or local storage

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+// Is the user using localStorage or firebase?
+
+
+
+
 // Function to toggle visiblity of main page when clicking buttons
 function toggleVisibility() {
     let blur = document.querySelectorAll(".to-blur");
@@ -22,14 +27,25 @@ closeFormBtn.addEventListener("click", () => {
 let formAddNewBook = document.querySelector("#add").addEventListener("click", addBookToLibrary);
 
 
-
-
 // Buttons that toggle visibility of main page
 // Add new book button
 document.querySelector("#add-book").addEventListener("click", () => {
     toggleVisibility();
     createForm();
 });
+
+// Selet the method to store data (local ou on database)
+function configFunc() {
+    let popup = document.querySelector(".pop-up-config-storage");
+    let styles = getComputedStyle(popup);
+    if (styles.display == "none") {
+        popup.style.display = "inline-block"
+    } else {
+        popup.style.display = "none"
+    }   
+    let blur = document.querySelectorAll(".config-to-blur");
+    blur.forEach(blur => blur.classList.toggle("active-blur"));
+}
 
 
 // Book storage functionality
@@ -51,10 +67,11 @@ function addBookToLibrary() {
     let bookCPages = document.querySelector("#cpages").value
     let bookReadingState = document.querySelector("#is-read").checked ? true : false;
     let newBook = new Book(bookTitle, bookAuthor, bookTPages, bookCPages, bookReadingState);
-    localStorage.setItem(`${bookTitle}Title`, bookTitle);
-    localStorage.setItem(`${bookTitle}Author`, bookAuthor);
-    localStorage.setItem(`${bookTitle}TPages`, bookTPages);
-    localStorage.setItem(`${bookTitle}CPages`, bookCPages);
-    localStorage.setItem(`${bookTitle}ReadingState`, bookReadingState);
+    myLibrary.push(newBook);
+    localStorage.setItem(bookTitle, JSON.stringify(myLibrary));
+    /* Para transformar de string de volta em objeto: SON.parse(localStorage.getItem("Dom Quitoxe"))[0]; */
+
 }
+
+
 

14bc70b3c5d1066bff731305e2e009a14928fa4e
VInícius <viniciultds2019@gmail.com>
2021-03-18 21:30:04 -0300

    Still needs to implement the edit button functionality on each button and firebase managemeant

--- js/script.js
+++ js/script.js
@@ -1,4 +1,65 @@
 // Is the user using localStorage or firebase?
+let userLocalStorage = "yes";
+let fireBase;
+
+let selectuserLocalStorage = document.querySelector("#local-storage");
+
+if (userLocalStorage == "yes") {
+    selectuserLocalStorage.setAttribute("style", "background-color: green", "color: white", "font-weight: bold")
+}
+
+if (localStorage.getItem("isLocal") == "yes") {
+    printAllBooks();
+}
+
+
+
+
+selectuserLocalStorage.addEventListener("click", () =>{
+    if (userLocalStorage == null) {
+        userLocalStorage = "yes"
+        localStorage.setItem("isLocal", userLocalStorage)
+        selectuserLocalStorage.setAttribute("style", "background-color: green", "color: white", "font-weight: bold")
+        fireBase = null;
+        localStorage.setItem("isFire", fireBase)
+        selectFireBase.setAttribute("style", "background-color: white", "color: black", "font-weight: normal")
+    } else if (userLocalStorage == "yes") {
+        userLocalStorage = null
+        localStorage.setItem("isLocal", userLocalStorage)
+        selectuserLocalStorage.setAttribute("style", "background-color: white", "color: black", "font-weight: normal")
+    }
+});
+
+let selectFireBase = document.querySelector("#cloud-storage");
+selectFireBase.addEventListener("click", () => {
+    if (fireBase == null) {
+        fireBase = "yes"
+        localStorage.setItem("isFire", fireBase)
+        selectFireBase.setAttribute("style", "background-color: green", "color: white", "font-weight: bold")
+        userLocalStorage = null;
+        localStorage.setItem("isLocal", userLocalStorage)
+        selectuserLocalStorage.setAttribute("style", "background-color: white", "color: black", "font-weight: normal")
+    } else if (fireBase == "yes") {
+        fireBase = null
+        localStorage.setItem("isFire", fireBase)
+        selectFireBase.setAttribute("style", "background-color: white", "color: black", "font-weight: normal")
+    }
+})
+
+let cancelSelection = document.querySelector("#cancel-selection");
+cancelSelection.addEventListener("click", () => {
+    userLocalStorage = null;
+    fireBase = null;
+    selectFireBase.setAttribute("style", "background-color: white", "color: black", "font-weight: normal")
+    selectuserLocalStorage.setAttribute("style", "background-color: white", "color: black", "font-weight: normal")
+    configFunc();
+})
+
+let closeSelection = document.querySelector("#close-window");
+closeSelection.addEventListener("click", () => {
+    configFunc();
+})
+
 
 
 
@@ -57,8 +118,10 @@ function Book(title, author, tpages, cpages, isRead) {
     this.author = author;
     this.tpages = tpages;
     this.cpages = cpages;
-    this.isRead = isRead ? true : false;
+    this.isRead = isRead ? "Read" : "Not Read";
 }
+
+let aBookName;
 
 function addBookToLibrary() {
     let bookTitle = document.querySelector("#title").value
@@ -68,10 +131,72 @@ function addBookToLibrary() {
     let bookReadingState = document.querySelector("#is-read").checked ? true : false;
     let newBook = new Book(bookTitle, bookAuthor, bookTPages, bookCPages, bookReadingState);
     myLibrary.push(newBook);
-    localStorage.setItem(bookTitle, JSON.stringify(myLibrary));
-    /* Para transformar de string de volta em objeto: SON.parse(localStorage.getItem("Dom Quitoxe"))[0]; */
+    let bookContent = document.createElement("div");
+    bookContent.classList.add("book-description");
+    bookContent.innerHTML = `
+        <div class="book-detail">BOOK'S TITLE<br>
+            <span class="book-specs" class="book-title">${newBook.title}</span>
+        </div>
+        <div class="book-detail">BOOK'S AUTHOR<br>
+            <span class="book-specs" class="book-title">${newBook.author}</span>
+        </div>
+        <div class="book-detail">NUMBER OF PAGES<br>
+            <span class="book-specs" class="book-total-pages">${newBook.tpages}</span>
+        </div>
+        <div class="book-detail">CURRENT PAGE<br>
+            <span class="book-specs" class="book-current-page">${newBook.cpages}</span>
+        </div>
+        <div class="book-detail">BOOK STATUS<br>
+            <span class="book-specs" class="book-status">${newBook.isRead}</span>
+        </div>
+        <div>
+            <button class="edit-book-button">Edit</button>
+        </div>
+    `;
+    document.querySelector("#library-elements-container").appendChild(bookContent);
+    localStorageSetting();
+}
 
+function localStorageSetting() {
+        let initialCounter = 0;
+        myLibrary.forEach((book) => {
+            localStorage.setItem(`${localStorage.length+1}${book}`, JSON.stringify(myLibrary[initialCounter]));
+            initialCounter += 1;
+        });
 }
 
 
 
+function printAllBooks() {
+    for (i = 0; i < localStorage.length; i++) {
+        if (localStorage.key(i).includes("object")) {
+            let bookContent = document.createElement("div");
+            bookContent.classList.add("book-description");
+            let bookToPrint = JSON.parse(localStorage.getItem(localStorage.key(i)))
+            bookContent.innerHTML = `
+            <div class="book-detail">BOOK'S TITLE<br>
+                <span class="book-specs" class="book-title">${bookToPrint.title}</span>
+            </div>
+            <div class="book-detail">BOOK'S AUTHOR<br>
+                <span class="book-specs" class="book-title">${bookToPrint.author}</span>
+            </div>
+            <div class="book-detail">NUMBER OF PAGES<br>
+                <span class="book-specs" class="book-total-pages">${bookToPrint.tpages}</span>
+            </div>
+            <div class="book-detail">CURRENT PAGE<br>
+                <span class="book-specs" class="book-current-page">${bookToPrint.cpages}</span>
+            </div>
+            <div class="book-detail">BOOK STATUS<br>
+                <span class="book-specs" class="book-status">${bookToPrint.isRead}</span>
+            </div>
+            <button class="book-buttons" id="edit-book-button">Edit</button>
+            <button class="book-buttons" id="delete-book-button" onclick="removeBook()">Delete</button>
+        `;
+        document.querySelector("#library-elements-container").appendChild(bookContent);
+    }
+    }
+}
+
+function removeBook () {
+    document.querySelector("#delete-book-button").parentElement.remove()
+}
